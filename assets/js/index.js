f595315d799812c1f6f0bff01335196d01b52608
Carlos Dip <cadudip@hotmail.com>
2019-11-12 08:18:18 -0300

    Finished fulscreen implementation, improved start screen.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-  let db = coDesConnect("https://codesign-2019-a.firebaseio.com/")
 
   let doc = document.querySelector('.main');
   doc.classList.add('hidden');
@@ -7,18 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
   let cover = document.querySelector('.cover');
   cover.classList.remove('hidden');
 
-  db.download('/', function(data) {
-    console.log(data);
-
-    let context = data;
-    coDesReplace('.menu', context);
-
-
-
-    let doc = document.querySelector('.main');
+  setTimeout(function(){
     doc.classList.remove('hidden');
-
-    let cover = document.querySelector('.cover');
     cover.classList.add('hidden');
-  })
-})
+  }, 0);
+
+  console.log('Time delay complete');
+
+  // Start code
+
+
+  // End code.
+
+})        

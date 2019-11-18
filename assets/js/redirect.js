ff799c76ac5e4693f9c60a24bd797f64b8e90abb
Carlos <cadudip@hotmail.com>
2019-11-18 19:51:21 -0300

    Improved entrada.html, and added screen confirming success.

---
+++ assets/js/redirect.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    setTimeout(function() {
+        window.location.href = "start.html"
+    }, 2000);
+
+    console.log('Time delay complete');
+
+    // Start code
+
+
+    // End code.
+
+})
